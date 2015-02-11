047eb0e7a6f059182552ef8c565beb4b3542e641
Seema <seemaullal@gmail.com>
2015-02-11 10:06:03 -0500

    Added all initial day routes for sending data and modifying database

--- routes/day.js
+++ routes/day.js
@@ -41,7 +41,8 @@ dayRouter.use('/:id', attractionRouter);
 
 // POST /days/:id/hotel
 attractionRouter.post('/hotel', function (req, res, next) {
-    var hotelID = req.body.hotel;
+    var hotelID = req.body.hotel._id;
+    var id = req.params.id;
     models.Day.findOne( {_id : id} , function(err, day) {
         day.hotel = hotelId;
     });
@@ -50,23 +51,48 @@ attractionRouter.post('/hotel', function (req, res, next) {
 // DELETE /days/:id/hotel
 attractionRouter.delete('/hotel', function (req, res, next) {
     var id = req.params.id;
+    models.Day.findOne( {_id : id} , function( err, day ) {
+        day.hotel = undefined;
+    });
 
 });
 // POST /days/:id/restaurants
 attractionRouter.post('/restaurants', function (req, res, next) {
     // creates a reference to a restaurant
+    var restID = req.body.restaurant._id;
+    var id = req.params.id;
+    models.Day.findOne( {_id : id} , function(err, day) {
+        day.restaurants.push(restID);
+    });
 });
 // DELETE /days/:dayId/restaurants/:restId
 attractionRouter.delete('/restaurant/:id', function (req, res, next) {
-    // deletes a reference to a restaurant
+    var id = req.params.id;
+    models.Day.findOne( {_id : id} , function( err, day ) {
+        var restaurantID = req.body.restaurant._id;
+        var index = day.restaurants.indexOf(restaurantID);
+        day.restaurants.splice(index,1);
+    });
 });
 // POST /days/:id/thingsToDo
 attractionRouter.post('/thingsToDo', function (req, res, next) {
     // creates a reference to a thing to do
+    var thingID = req.body.thing._id;
+    var id = req.params.id;
+    models.Day.findOne( {_id : id} , function(err, day) {
+        day.thingsToDo.push(thingID);
+    });
+
 });
 // DELETE /days/:dayId/thingsToDo/:thingId
 attractionRouter.delete('/thingsToDo/:id', function (req, res, next) {
     // deletes a reference to a thing to do
+    var id = req.params.id;
+    models.Day.findOne( {_id : id} , function( err, day ) {
+        var thingID = req.body.thing._id;
+        var index = day.thingsToDo.indexOf(thingID);
+        day.thingsToDo.splice(index,1);
+    });
 });
 
 module.exports = dayRouter;
